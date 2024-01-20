860defa4dae7326f4ffce4fd9c38bce43090bc6d
Niels <ndhollander1@gmail.com>
2024-01-20 23:02:57 +0100

    finished input matrix

--- classes/UI_stuff/input_matrix.py
+++ classes/UI_stuff/input_matrix.py
@@ -1,6 +1,6 @@
-from classes.style import Colours
 import pygame as pg
 import numpy as np
+from random import randint
 
 class Input_Matrix:
     
@@ -94,7 +94,55 @@ class Input_Matrix:
         pg.draw.line(window.screen, window.colours.shade_neg, (self.x, self.y + self.height), (self.x + self.width, self.y + self.height), 2)
         pg.draw.line(window.screen, window.colours.shade_neg, (self.x + self.width, self.y), (self.x + self.width, self.y + self.height), 2)
         
+    
+    
+    def draw_cells(self, screen):
         
+        for row, col in np.ndindex(self.matrix.shape):
+            strength = abs(self.matrix[row, col]) * 255
+            colour = (0,strength,0) if self.matrix[row, col] > 0 else (strength,0,0) # green if positive, red if negative
+            pg.draw.rect(screen, colour, (self.x + col * self.size + 1,
+                                                 self.y + row * self.size + 1,
+                                                 self.size - (self.dim + 1) / self.dim,
+                                                 self.size - (self.dim + 1) / self.dim))
+            
+    
+    
+    def draw_colourballs(self, screen):
+        
+        for i in range(self.dim):
+            
+            # determine colour
+            colour = pg.Color(0,0,0); colour.hsla = (360 * i / self.dim, 100, 50, 100)
+            
+            # draw horizontal balls
+            pg.draw.circle(screen, colour,
+                           (self.x + i * self.size + self.size / 2,
+                            self.y - self.height / 20),
+                           self.height / 40)
+            
+            # draw vertical balls
+            pg.draw.circle(screen, colour,
+                           (self.x - self.width / 20,
+                            self.y + i * self.size + self.size / 2),
+                           self.width / 40)
+        
+    
+    
+    def animate_rnd_matrix(self):
+        
+        # get random matrix element
+        coord = (randint(0,self.dim - 1), randint(0,self.dim - 1))
+        
+        # if number of elapsed seconds is uneven, increase element if possible
+        if (pg.time.get_ticks()//1000) % 2 and self.matrix[coord] < 0.9:
+            self.matrix[coord] += 0.1
+        
+        # otherwise decrease if possible
+        elif self.matrix[coord] > -0.9:
+            self.matrix[coord] -= 0.1
+    
+    
     
     def draw(self, window):
         
@@ -108,20 +156,21 @@ class Input_Matrix:
         # draw grid
         pg.draw.rect(window.screen, window.colours.underline,(self.x, self.y, self.width, self.height))
         
+        # draw cells
+        self.draw_cells(window.screen)
+        
+        # draw coloured balls
+        self.draw_colourballs(window.screen)
+        
         # add shading to grid
         self.shade_grid(window)
         
-        # draw cells
-        for row, col in np.ndindex(self.matrix.shape):
-            strength = abs(self.matrix[row, col]) * 255
-            colour = (0,strength,0) if self.matrix[row, col] > 0 else (strength,0,0)
-            pg.draw.rect(window.screen, colour, (self. x + col * self.size + 1,
-                                                 self.y + row * self.size + 1,
-                                                 self.size - (self.dim + 1) / self.dim,
-                                                 self.size - (self.dim + 1) / self.dim))
-        
-        # if not locked, allow user input
+        # if matrix not locked, allow user input
         if not self.locked:
             self.check_mouseover(window, grid_rect)
+        
+        # if matrix is locked, animate 'randomisation' of matrix
+        else:
+            self.animate_rnd_matrix()
         
         return self.matrix
